2c52e8b40c198d5dd5c4d5f4ea496f80b3577b29
pablopedrosa <pablopedrosap@gmail.com>
2023-06-15 09:19:31 +0200

    Final version

--- main.py
+++ main.py
@@ -1,44 +1,56 @@
 class Checkout:
+    """Checkout system that can scan items and apply pricing rules."""
     def __init__(self, pricing_rules):
+        """Initialize a Checkout object with the pricing rules."""
         self.pricing_rules = pricing_rules
-        self.cart = []
+        self.cart = {}
 
     def scan(self, item):
-        self.cart.append(item)
+        """Scan an item and add it to the cart."""
+        if item in self.cart:
+            self.cart[item] += 1
+        else:
+            self.cart[item] = 1
 
     def calculate_total(self):
+        """Applying any pricing rules, calculate the total price of the items in the cart."""
         total = 0
-        voucher_count = self.cart.count('VOUCHER')
-        tshirt_count = self.cart.count('TSHIRT')
-        pants_count = self.cart.count('PANTS')
-
-        # Apply 2-for-1 special on VOUCHER
-        total += (voucher_count // 2 + voucher_count % 2) * self.pricing_rules['VOUCHER']
-
-        # Apply discount on TSHIRT
-        if tshirt_count >= 3:
-            total += tshirt_count * 19
-        else:
-            total += tshirt_count * self.pricing_rules['TSHIRT']
-
-        # Add PANTS items to total
-        total += pants_count * self.pricing_rules['PANTS']
-
+        for item, count in self.cart.items():
+            if item in self.pricing_rules:
+                total += self.pricing_rules[item](count)
+            else:
+                raise ValueError(f'Pricing rule not found for item: {item}')
         return total
+# Pricing rules
+
+
+def voucher_rule(quantity):
+    """Pricing rule for VOUCHER: 2-for-1."""
+    return (quantity // 2 + quantity % 2) * 5.00
+
+
+def tshirt_rule(quantity):
+    """Pricing rule for TSHIRT: discount for 3 or more."""
+    if quantity >= 3:
+        return quantity * 19.00
+    else:
+        return quantity * 20.00
+
+
+def pants_rule(quantity):
+    """Pricing rule for PANTS: no discount."""
+    return quantity * 7.50
 
 
 pricing_rules = {
-    'VOUCHER': 5,
-    'TSHIRT': 20,
-    'PANTS': 7.5
+    'VOUCHER': voucher_rule,
+    'TSHIRT': tshirt_rule,
+    'PANTS': pants_rule
 }
 
-checkout = Checkout(pricing_rules)
-while True:
-    product = input('Select voucher/tshirt/pants or 999 to finish order: ').upper()
-    if product == '999':
-        break
-    else:
-        checkout.scan(product)
+co = Checkout(pricing_rules)
+co.scan('VOUCHER')
+co.scan('TSHIRT')
+co.scan('PANTS')
+print(f'Total:  {co.calculate_total()}€')
 
-print(checkout.calculate_total())
